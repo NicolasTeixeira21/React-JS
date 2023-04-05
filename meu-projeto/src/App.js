0b77a7f10f20d039904079e1bf0237e10e02c552
Nicolas Teixeira <104282872+NicolasTeixeira21@users.noreply.github.com>
2023-04-05 20:34:07 -0300

    atualizado

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,12 +1,21 @@
 import './App.css';
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
 
 function App() {
+  const nome = 'Maria'
   return (
     <div className="App">
-      <h1>Olá, Mundo!</h1>
-      <p>Meu primeiro App</p>
+      <SayMyName nome="Nicolas"/>
+      <SayMyName nome="Fred"/>
+      <SayMyName nome={nome}/>
+      <Pessoa 
+      nome="Nicolas" 
+      idade="21" 
+      profissao="Programador" 
+      foto="https://via.placeholder.com/150"/>
     </div>
-  );
+  )
 }
 
 export default App;
