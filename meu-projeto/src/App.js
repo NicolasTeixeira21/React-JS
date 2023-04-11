b0eff1eb4c04c4f6a784413da58cee458e10bfc2
Nicolas Teixeira <104282872+NicolasTeixeira21@users.noreply.github.com>
2023-04-10 21:51:51 -0300

    Update

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa'
+import Frase from './components/Frase'
 
 function App() {
   const nome = 'Maria'
   return (
     <div className="App">
+      <h1>Testando CSS</h1>
+      <Frase/>
+      <Frase/>
       <SayMyName nome="Nicolas"/>
       <SayMyName nome="Fred"/>
       <SayMyName nome={nome}/>
